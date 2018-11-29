691c124ae87831a9baaefd9e37fe69b22628bf3f
CarlosPadilla <carlosp@bluetrailsoft.com>
2018-11-29 09:51:40 -0600

    min change

--- backend/views/pelicula/_form.php
+++ backend/views/pelicula/_form.php
@@ -10,32 +10,32 @@ use yii\widgets\ActiveForm;
 
 <div class="pelicula-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin();?>
 
-    <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
+    <?php echo $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'genero')->textInput(['maxlength' => true]) ?>
+    <?php echo $form->field($model, 'genero')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'calificacion')->textInput(['type'=> 'number', "min"=>"0", "max"=>"5"]) ?>
+    <?php echo $form->field($model, 'calificacion')->textInput(['type' => 'number', "placeholder" => "0.0", "min" => "0", "max" => "5", "step" => "0.5"]) ?>
 
-    <?= $form->field($model, 'clasificacion')->dropDownList(['AA', 'A', 'B', 'B15', 'C', 'D'], ['prompt' => 'clasificación']) ?>
+    <?php echo $form->field($model, 'clasificacion')->dropDownList(['AA' => 'AA', 'A' => 'A', 'B' => 'B', 'B15' => 'B15', 'C' => 'C', 'D' => 'D'], ['prompt' => 'clasificación']) ?>
 
-    <?= $form->field($model, 'idioma')->textInput(['maxlength' => true]) ?>
+    <?php echo $form->field($model, 'idioma')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'duracion')->textInput(['maxlength' => true]) ?>
+    <?php echo $form->field($model, 'duracion')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'sinopsis')->textarea(['rows' => 6]) ?>
+    <?php echo $form->field($model, 'sinopsis')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'cartelUrl')->textarea(['rows' => 6]) ?>
+    <?php echo $form->field($model, 'cartelUrl')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'trailerUrl')->textarea(['rows' => 6]) ?>
+    <?php echo $form->field($model, 'trailerUrl')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'trailerImg')->textarea(['rows' => 6]) ?>
+    <?php echo $form->field($model, 'trailerImg')->textarea(['rows' => 6]) ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
+        <?php echo Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
     </div>
 
-    <?php ActiveForm::end(); ?>
+    <?php ActiveForm::end();?>
 
 </div>
