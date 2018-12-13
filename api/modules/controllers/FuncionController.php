b0e6969c7b23f873fb8b473a71e4095a15b5f6f1
CarlosPadilla <carlosp@bluetrailsoft.com>
2018-12-12 23:14:56 -0600

    z

--- api/modules/controllers/FuncionController.php
+++ api/modules/controllers/FuncionController.php
@@ -14,6 +14,7 @@ class FuncionController extends BaseController
     {
         return [
             'index',
+            'ping',
             'options' => [
                 'class' => 'yii\rest\OptionsAction',
             ],
